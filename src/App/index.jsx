7c6efb0f28fc7bb81438a24fa058ddc698985dc9
Hadi Tabatabaei <hadi.tabatabaei@divar.ir>
2024-01-06 01:35:00 +0330

    feat: Add initial project structure

---
+++ src/App/index.jsx
@@ -0,0 +1,12 @@
+import sampleImage from "@assets/vite.svg";
+
+function App() {
+  return (
+    <>
+      <p>Hello World</p>
+      <img src={sampleImage} />
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@configs': path.resolve(__dirname, './src/configs'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@pages': path.resolve(__dirname, './src/pages')
+    }
+  },
+  plugins: [react()],
+
+})
