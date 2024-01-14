e2b728a4214af44d79c372a9281be892b40f295d
AminAnsariMehr <139113306+AminAnsariMehr@users.noreply.github.com>
2024-01-14 16:54:45 +0330

    feat(styles): Add css spacing variables
    Add css spacing variables

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import '@assets/styles/spacing.css'
 
 const root = createRoot(document.getElementById('root'))
 root.render(
