49b18379ff15ae3117af96b019ce9d1e3006f77f
Danial <aamyry780@gmail.com>
2024-01-14 23:58:58 +0330

    fixt conflict for Merge branch

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-<<<<<<< HEAD
 import '@assets/styles/colors.css'
-=======
 import '@assets/styles/spacing.css'
->>>>>>> 0b2a5c128267067ec464316036bbd75cd0e2066c
-
 const root = createRoot(document.getElementById('root'))
 root.render(
   <StrictMode>
