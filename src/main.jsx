cc53d8f0134c1e08d23769f5d448d208397eedff
majid-ahangaran <139076268+majid-ahangaran@users.noreply.github.com>
2024-01-16 02:32:12 +0330

    feat: create sizes.css file and coding font size variables (#10)
    * feat: create sizes.css file and coding font size variables
    
    * fontsizechange
    
    * importfontsizes
    
    ---------
    
    Co-authored-by: Hadi Tabatabaei <hadi.tabatabaei@divar.ir>

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import '@assets/styles/sizes.css'
 import '@assets/styles/colors.css'
 import '@assets/styles/spacing.css'
+
 const root = createRoot(document.getElementById('root'))
 root.render(
   <StrictMode>
