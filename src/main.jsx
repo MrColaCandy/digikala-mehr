388e79941cee7edd52045a224673ac5c31e582fd
Elham-Arabi <140654989+Elham-Arabi@users.noreply.github.com>
2024-01-26 00:09:31 +0330

    fix: Fix RTL fonts and reset
    * add changes request
    
    * fix
    
    ---------
    
    Co-authored-by: Hadi Tabatabaei <hadi.tabatabaei@divar.ir>

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,8 @@ import '@assets/styles/sizes.css'
 import '@assets/styles/colors.css'
 import '@assets/styles/spacing.css'
 import "@assets/styles/reset.css"
-import "@assets/styles/Yekan.css"
+import "@assets/styles/font.css"
+import "@assets/styles/rtl.css"
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
