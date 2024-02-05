717730bea071fc7f90b23affe4a3bbb95dbb69ea
Mahmood <mahmoodkhodadady10@gmail.com>
2024-02-05 17:52:59 +0330

    Refactored Slider

--- src/pages/ChoosePlan/ChoosePlanSlider/index.jsx
+++ src/pages/ChoosePlan/ChoosePlanSlider/index.jsx
@@ -4,28 +4,25 @@ import './style.css';
 
 
 
-const ChoosePlaneSlider = ({ children,setCurrentProject, gap, currentSlide , setCurrentSlide = () => { }, variant }) => {
+const ChoosePlaneSlider = ({ children, setCurrentProject, gap, currentSlide=1, setCurrentSlide = () => { }, variant }) => {
     const containerRef = useRef(null);
     const previousButton = useRef(null);
     const nextButton = useRef(null);
     const [slideWidth, setSlideWidth] = useState(null);
     const [slideHeight, setSlideHeight] = useState(null);
-    const [totalSlides, setTotalSlides] = useState(0);
+    const [slides, setSlides] = useState(null);
     const [index,setIndex]=useState(0);
-    function getSlideRect() {
-        return containerRef.current.querySelector("#slide").getBoundingClientRect();
-    }
+  
     function updateSlideDimensions() {
-        const { width, height } = getSlideRect()
+        const { width, height } = containerRef.current.querySelector("#slide").getBoundingClientRect()
         setSlideWidth(width);
         setSlideHeight(height);
     }
-   
 
+    
     useEffect(() => {
         if (!containerRef.current) return;
-        previousButton.current.style.display="none";
-        setTotalSlides(containerRef.current.querySelectorAll("#slide").length)
+        previousButton.current.style.display = "none";
         updateSlideDimensions();
         const resizeObserver = new ResizeObserver(() => {
             updateSlideDimensions();
@@ -35,28 +32,40 @@ const ChoosePlaneSlider = ({ children,setCurrentProject, gap, currentSlide , set
             updateSlideDimensions();
             containerRef.current.scrollLeft = 0;
             setCurrentSlide(1)
+            setIndex(0);
         })
         containerRef.current.onscroll = () => {
-            const atEnd=(containerRef.current.scrollWidth + containerRef.current.scrollLeft - containerRef.current.getBoundingClientRect().width)<20;
-            const atStart=containerRef.current.scrollLeft>-1;
-            nextButton.current.style.display = atEnd? "none":"flex"
-            previousButton.current.style.display=atStart?"none":"flex";
-            const slides = document.querySelectorAll('#slide');
-
+            const atEnd = (containerRef.current.scrollWidth + containerRef.current.scrollLeft - containerRef.current.getBoundingClientRect().width) < 20;
+            const atStart = containerRef.current.scrollLeft > -1;
+            nextButton.current.style.display = atEnd ? "none" : "flex"
+            previousButton.current.style.display = atStart ? "none" : "flex";
             
+
+            if(!slides)return;
             for (let i = 0; i < slides.length; i++) {
                 let slide = slides[i];
                 if (isScrolledIntoView(slide)) {
-                    
-                    let currentSlideNumber = i + 1; 
+
+                    let currentSlideNumber = i + 1;
                     setCurrentSlide(currentSlideNumber);
                     setCurrentProject(slide.dataset.project);
                 }
             }
         }
     }, []);
+
+    useEffect(()=>{
+        setSlides([...containerRef.current.querySelectorAll("#slide")]);
+    },[])
   
-    
+    useEffect(()=>{
+        if(!slides)return;
+        slides[index].scrollIntoView({
+            behavior: 'smooth',
+            block: 'nearest',
+            inline: 'center'
+        })
+    },[index,slides])
     return (
         <div className='slider'>
             <div className={`slider__view--${variant}`} style={{ "--slide-width": `${slideWidth}px`, "--slide-height": `${slideHeight}px`, "--slides-gap": `${gap}px` }}>
@@ -66,27 +75,21 @@ const ChoosePlaneSlider = ({ children,setCurrentProject, gap, currentSlide , set
             </div>
             <button ref={el => nextButton.current = el} className={`slider__nextButton--${variant}`} onClick={() => {
 
-                
                 setCurrentSlide(currentSlide + 1)
-                const slides= containerRef.current.querySelectorAll("#slide");
-                slides[index+1]?.scrollIntoView({  
-                behavior: 'smooth',
-                block: 'nearest',
-                inline: 'center'})
-                setIndex((index+1)%totalSlides)
+                
+                setIndex(prev=>prev+1)
+               
 
             }}>
                 <FaChevronLeft size={"20px"} />
             </button>
             <button ref={el => previousButton.current = el} className={`slider__previousButton--${variant}`} onClick={() => {
-               
+
                 setCurrentSlide(currentSlide - 1)
-                const slides= containerRef.current.querySelectorAll("#slide");
-                slides[index-1]?.scrollIntoView({  
-                    behavior: 'smooth',
-                    block: 'nearest',
-                    inline: 'center'})
-                setIndex((index-1)%totalSlides)
+               
+                setIndex(prev=>prev-1)
+                
+                
             }}>
                 <FaChevronRight size={"20px"} />
             </button>
