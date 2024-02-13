b8f7abf78709a67285bd47b2bb718034776d9beb
Mahmood <mahmoodkhodadady10@gmail.com>
2024-02-13 17:25:45 +0330

    Changed Choose plan card header image

--- src/pages/ChoosePlan/ChoosePlanCard/index.jsx
+++ src/pages/ChoosePlan/ChoosePlanCard/index.jsx
@@ -1,31 +1,32 @@
-import {  useState } from 'react';
+import { useState } from 'react';
 import ChoosePlanTextBox from "../ChoosePlanTextBox"
 import "./style.css"
 import ChoosePlanButton from '../ChoosePlanButton';
-const ChoosePlanCard = ({id,title,subtitle,bodyText,image,subtitleImage}) => {
-    const subtitleImageUrl = new URL(subtitleImage, import.meta.url)
-    const ImageUrl = new URL(image, import.meta.url)
+const ChoosePlanCard = ({ id, title, subtitle, bodyText, image, subtitleImage }) => {
+    
+    
     const [showFullText, setShowFullText] = useState(false);
-   
+
     return (
         <section id='slide' data-project={id} className={`choosePlanCard`}>
             {
                 !showFullText &&
-                <div className={`choosePlanCard__image`} style={{ "--card-image": `url('${ImageUrl}')` }} >
-                    
-                        
-                    <div className="choosePlanCard__imageHeader">{title}</div>
-                    
-                    <div className="choosePlanCard__logo">
-                        <img src={subtitleImageUrl} className="choosePlanCard__logoImage" />
-                        <div className="choosePlanCard__logoTitle">{subtitle}</div>
+                <div className='choosePlanCard__imageWrapper'>
+                    <img src={image} className="choosePlanCard__image" />
+                    <div className='choosePlanCard__imageContent'>
+                        <div className="choosePlanCard__imageHeader">{title}</div>
+                        <div className="choosePlanCard__logo">
+                            <img src={subtitleImage} className="choosePlanCard__logoImage" />
+                            <div className="choosePlanCard__logoTitle">{subtitle}</div>
+                        </div>
                     </div>
                 </div>
+
             }
             <div className={`choosePlaneCard__header`}>{title}</div>
-            <ChoosePlanTextBox  setShowFullText={setShowFullText} showFullText={showFullText} text={bodyText} />
+            <ChoosePlanTextBox setShowFullText={setShowFullText} showFullText={showFullText} text={bodyText} />
             <ChoosePlanButton projectId={id} />
-            
+
         </section>
     )
 }
--- src/pages/ChoosePlan/ChoosePlanSlider/index.jsx
+++ src/pages/ChoosePlan/ChoosePlanSlider/index.jsx
@@ -4,32 +4,24 @@ import './style.css';
 
 
 
-const ChoosePlaneSlider = ({  scrollBehavior="smooth",children, gap, currentSlide, setCurrentSlide }) => {
+const ChoosePlaneSlider = ({ scrollBehavior = "smooth", children, gap, currentSlide, setCurrentSlide }) => {
     const containerRef = useRef(null);
     const previousButton = useRef(null);
     const nextButton = useRef(null);
     const [width, setWidth] = useState(0);
     const [height, setHeight] = useState(0);
-    const [slides,setSlides]=useState(null);
-    const [isMediaMatches,setIsMediaMatches]=useState(window.matchMedia("(max-width: 390px)").matches);
-    useEffect(()=>{
-     window.addEventListener("resize",()=>{
-        setIsMediaMatches(window.matchMedia("(max-width: 390px)").matches);
-     })
-    },[])
+
+
+
     function updateSlideDimensions() {
         const { width, height } = containerRef.current.querySelector("#slide").getBoundingClientRect()
         setWidth(width);
         setHeight(height);
     }
-    function isInView(element)
-    {   const rect=element.getBoundingClientRect();
-        return rect.right <= window.innerWidth 
-    }
-    
+
+
     useEffect(() => {
         if (!containerRef.current) return;
-        setSlides([...containerRef.current.querySelectorAll("#slide")]);
         updateSlideDimensions();
         const resizeObserver = new ResizeObserver(() => {
             updateSlideDimensions();
@@ -37,53 +29,47 @@ const ChoosePlaneSlider = ({  scrollBehavior="smooth",children, gap, currentSlid
         resizeObserver.observe(containerRef.current.querySelector("#slide"));
         window.addEventListener("resize", () => {
             updateSlideDimensions();
-            containerRef.current.scrollLeft=0;
-            setCurrentSlide(0);
-            nextButton.current.style.display="flex";
-            previousButton.current.style.display="none";
         })
         previousButton.current.style.display = "none";
+      
         containerRef.current.addEventListener("scroll", () => {
             const atEnd = (containerRef.current.scrollWidth + containerRef.current.scrollLeft - containerRef.current.getBoundingClientRect().width) < 20;
             const atStart = containerRef.current.scrollLeft > -1;
             nextButton.current.style.display = atEnd ? "none" : "flex"
-            previousButton.current.style.display = atStart ? "none" : "flex";
+            previousButton.current.style.display = atStart ? "none" : "flex"
+         
         })
+
         containerRef.current.addEventListener("scrollend", () => {
-            if(!isMediaMatches)return;
-            setTimeout(() => {
-            const slides=containerRef.current.querySelectorAll("#slide")
-            for (let index = 0; index < slides.length; index++) {
-                const slide = slides[index];
-                if (isInView(slide)) {
-                    setCurrentSlide(index);
-                    break;
-                }
-               
-            }
-            }, 200);
-           
+            const slides=[...containerRef.current.querySelectorAll("#slide")];
+            const slidesLength=slides.length;
+            const scrollPercentage = Math.abs(Math.ceil(100 * containerRef.current.scrollLeft / (containerRef.current.scrollWidth-containerRef.current.clientWidth))); 
+            const currentIndex = Math.floor(scrollPercentage / (100 / slidesLength));
+            setCurrentSlide(currentIndex)
+
         })
     }, []);
     useEffect(() => {
-        if(!slides)return;
-        slides[currentSlide].scrollIntoView({inline:"center",block:"center"})
+        const slides = containerRef.current.querySelectorAll("#slide")
+        slides[currentSlide]?.scrollIntoView({ inline: "center", block: "center" })
 
-    }, [currentSlide,slides])
+    }, [currentSlide])
     return (
         <div className='slider'>
-            <div className={`slider__view`} style={{ "--slide-width": `${width}px`, "--slide-height": `${height}px`, "--slides-gap": `${gap}px`,"--scroll-behavior":`${scrollBehavior}` }}>
+            <div className={`slider__view`} style={{ "--slide-width": `${width}px`, "--slide-height": `${height}px`, "--slides-gap": `${gap}px`, "--scroll-behavior": `${scrollBehavior}` }}>
                 <div id="slider-container" className={`slider__container`} ref={containerRef}>
                     {children}
                 </div>
             </div>
             <button ref={nextButton} className={`slider__nextButton`} onClick={() => {
+
                 setCurrentSlide(prevSlide => prevSlide + 1);
-                console.log(currentSlide);
+
             }}>
                 <FaChevronLeft size={"20px"} />
             </button>
             <button ref={previousButton} className={`slider__previousButton`} onClick={() => {
+
                 setCurrentSlide(prevSlide => prevSlide - 1);
 
             }}>
@@ -91,7 +77,7 @@ const ChoosePlaneSlider = ({  scrollBehavior="smooth",children, gap, currentSlid
             </button>
         </div>
     );
-};
+}
 
 
 
