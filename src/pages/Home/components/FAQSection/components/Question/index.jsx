a1b91293030628938d9832f0001ded7b29eec89c
Mahmood <mahmoodkhodadady10@gmail.com>
2024-02-14 20:24:51 +0330

    Add FAQ functionality

--- src/pages/Home/components/FAQSection/components/Question/index.jsx
+++ src/pages/Home/components/FAQSection/components/Question/index.jsx
@@ -1,25 +1,27 @@
+import { useState } from "react";
 import { FaChevronDown } from "react-icons/fa6";
 import "./style.css"
-import { useState } from "react";
-const Question = ({text}) => {
-    const [showAnswer,setShowAnswer]=useState(false);
+const Question = ({ questionText,answerText }) => {
+    const [showAnswer, setShowAnswer] = useState(false);
 
-    function handleQuestionIconClick()
-    {
+    function handleQuestionIconClick() {
         setShowAnswer(!showAnswer);
+
     }
 
     return (
-        <div className="FAQ__questionItem">
-            <div className="FAQ__questionPart">
-                <span className="FAQ__questionText">
-                    مبالغ جمع شده صرف چه پروژه هایی خواهند شد؟ و چطور از این کارهای خیرخواهانه با خبر بشم؟
-                </span>
-                <p className="FAQ__answerText"></p>
-                <FaChevronDown onClick={handleQuestionIconClick} className="FAQ__questionIconDown" />
+        <>
+            <div className={`question--${showAnswer ? "answer" : "noAnswer"}`}>
+                    <div className="question__text">
+                        {questionText}
+                        <FaChevronDown onClick={handleQuestionIconClick} className="question__icon" />
+                    </div>
+                    <div className={`question__answer--${showAnswer ? "show" : "hide"}`}>
+                       {answerText}
+                    </div>
             </div>
-            
-        </div>
+
+        </>
     )
 }
 
