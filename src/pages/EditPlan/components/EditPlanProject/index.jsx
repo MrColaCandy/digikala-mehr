56f4e0b91277ab4c5065bcd1494a4ed0aca3e4d3
Mahmood <mahmoodkhodadady10@gmail.com>
2024-02-20 12:57:21 +0330

    fixed edit price link

--- src/pages/EditPlan/components/EditPlanProject/index.jsx
+++ src/pages/EditPlan/components/EditPlanProject/index.jsx
@@ -1,32 +1,29 @@
 import { FaChevronLeft } from "react-icons/fa6";
 import usePersianNumberConverter from "@components/hooks/usePersianNumberConverter";
-import {useAuth} from "@components/hooks/useAuth"
+import { useAuth } from "@components/hooks/useAuth"
 import "./style.css"
 import { useEffect } from "react";
-import {useNavigate} from "react-router-dom"
-const EditPlanProject = ({ index,project,selected,setSelected }) => {
-    const navigate =useNavigate()
-    const {user,setUser}=useAuth()
-    function handleEditPriceClick()
-    {
-         setUser({...user,editing:project})
-         localStorage.setItem("user",JSON.stringify(user));
-         navigate("/edit-price")
+import { useNavigate } from "react-router-dom"
+const EditPlanProject = ({ index, project, selected, setSelected }) => {
+    const navigate = useNavigate()
+    const { user, setUser } = useAuth()
+    function handleEditPriceClick() {
+        setUser({ ...user, editing: project })
+        localStorage.setItem("user", JSON.stringify(user));
+        navigate("/edit-price")
     }
     const { convert, addCommas } = usePersianNumberConverter();
-    useEffect(()=>{
-        if(index===0)
-        {
+    useEffect(() => {
+        if (index === 0) {
             setSelected(project)
         }
-    },[index])
-    function handleProjectClick()
-    {
-        
+    }, [index])
+    function handleProjectClick() {
+
         setSelected(project);
     }
     return (
-        <div onClick={handleProjectClick} className={`editPlanProject${selected?.id===project?.id?"--selected":""}`}>
+        <div onClick={handleProjectClick} className={`editPlanProject${selected?.id === project?.id ? "--selected" : ""}`}>
             <div className="editPlanProject__header">
 
                 <img src={project?.employerLogo} className="editPlanProject__employerLogo" />
@@ -34,23 +31,25 @@ const EditPlanProject = ({ index,project,selected,setSelected }) => {
                     <div className="editPlanProject__employerName">{project?.employerName}</div>
                     <div className="editPlanProject__projectTitle">{project?.title}</div>
                 </div>
-                <div className={`editPlanProject__price${selected?.id===project?.id?"--selected":""}`}>
+                <div className={`editPlanProject__price${selected?.id === project?.id ? "--selected" : ""}`}>
                     ماهیانه
                     مبلغ
-                    {" "+project ? convert(addCommas(project.price)) : 0+" "}
+                    {" " + project ? convert(addCommas(project.price)) : 0 + " "}
                     تومان
                 </div>
             </div>
             <div className="editPlanProject__description">
-            <div className="editPlanProject__text">
-                {
-                    project?.description
-                }
-            </div>
-            <div onClick={handleEditPriceClick} className="editPlanProject__textGreen">
-                <span>برای تغییر مبلغ پرداختی ماهیانه اینجا کلید کنید</span>
-                <FaChevronLeft size={12} />
-            </div>
+                <div className="editPlanProject__text">
+                    {
+                        project?.description
+                    }
+                </div>
+                <div  className="editPlanProject__textGreen">
+                    <div className="editPlanProject__editPrice"  onClick={handleEditPriceClick}>
+                        <span>برای تغییر مبلغ پرداختی ماهیانه اینجا کلید کنید</span>
+                        <FaChevronLeft size={12} />
+                    </div>
+                </div>
             </div>
         </div>
     )
