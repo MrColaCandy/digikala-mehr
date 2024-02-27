3d1caa55fc851e9ea0124c32fd8908ef182e9756
Mahmood <mahmoodkhodadady10@gmail.com>
2024-02-27 17:26:33 +0330

    fixed minor issue in page access

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -39,7 +39,7 @@ const Layout = ({ children }) => {
         {
             navigate("/profile")
         }
-        if(location.pathname=="/choose-price" && !parse(document.cookie).projectId)
+        if(location.pathname=="/choose-price" && parse(document.cookie).projectId==="null")
         {
             navigate("/choose-plan")
         }
@@ -47,7 +47,7 @@ const Layout = ({ children }) => {
         {
             navigate("/profile")
         }
-        if(location.pathname=="/edit-price" && (!parse(document.cookie).editing || !activeProject))
+        if(location.pathname=="/edit-price" && (parse(document.cookie).editing==="null" || !activeProject))
         {
             navigate("/profile")
         }
