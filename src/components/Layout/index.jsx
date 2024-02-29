b59b9667f876258dab733f940ff9befed1752713
Mahmood <mahmoodkhodadady10@gmail.com>
2024-02-29 09:35:18 +0330

    fixed page access issue

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -4,64 +4,57 @@ import { useProject } from "../hooks/useProject"
 import { useEffect } from "react";
 import { useAuth } from "../hooks/useAuth";
 import { useNavigate } from "react-router-dom"
-import {useLocation} from "react-router-dom"
+import { useLocation } from "react-router-dom"
 import { parse } from "cookie";
 const Layout = ({ children }) => {
-    const { setUser, getUser,activeProject,getActiveProject,setActiveProject } = useProject();
+    const { setUser, getUser, getActiveProject } = useProject();
     const { logout, setIsLoggedIn, token } = useAuth()
     const navigate = useNavigate();
-    const location=useLocation();
+    const location = useLocation();
     async function getUserOnLoad() {
         if (!token || token == "") {
             logout();
             return;
         }
         try {
-            
+
             const user = await getUser();
             setUser(user);
             setIsLoggedIn(true);
         } catch (error) {
             logout();
             navigate("/");
-            
+
         }
     }
-    async function getActiveProjectOnLoad()
-    {
+    async function getActiveProjectOnLoad() {
         try {
-            const activeProject=await getActiveProject();
-            setActiveProject(activeProject);
+            const activeProject = await getActiveProject();
+            if (location.pathname == "/choose-plan" && activeProject) {
+                navigate("/profile")
+            }
+            if (location.pathname == "/choose-price" && (parse(document.cookie).projectId == "" || activeProject)) {
+                navigate("/profile")
+            }
+            if (location.pathname == "/edit-plan" && !activeProject) {
+                navigate("/profile")
+            }
+            if (location.pathname == "/edit-price" && (parse(document.cookie).editing == "" || !activeProject)) {
+                navigate("/profile")
+            }
         } catch (error) {
-            setActiveProject(null);
+            console.log(error);
+            navigate("/");
         }
     }
     useEffect(() => {
-        const abortController= new AbortController();
+        const abortController = new AbortController();
         getUserOnLoad();
         getActiveProjectOnLoad();
-        return ()=>abortController.abort();
-    }, [])
+        return () => abortController.abort();
+    }, [location])
 
-    useEffect(()=>{
-        console.log({location:location.pathname,activeProject:activeProject});
-        if(location.pathname=="/choose-plan" && activeProject)
-        {
-            navigate("/profile")
-        }
-        if(location.pathname=="/choose-price" && parse(document.cookie).projectId=="")
-        {
-            navigate("/choose-plan")
-        }
-        if(location.pathname=="/edit-plan" && !activeProject)
-        {
-            navigate("/profile")
-        }
-        if(location.pathname=="/edit-price" && (parse(document.cookie).editing=="" || !activeProject))
-        {
-            navigate("/profile")
-        }
-    },[location])
+
 
     return (
         <>
